ad4586806414cab4a52673106b69defad542f5a9
”Vadim <vadimka.gapon@gmail.com>
2022-04-14 22:30:10 +0600

    added tests for login page

---
+++ test_login_page.py
@@ -0,0 +1,13 @@
+from .pages.login_page import LoginPage
+    
+def test_should_be_register_form(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+    
+def test_should_be_login_url(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
